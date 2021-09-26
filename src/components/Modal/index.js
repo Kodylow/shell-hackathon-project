056ec050027cc842fd69eaa65c053fde482d44e7
Daniel Marchan <dmarchan@dmsgroup.com>
2021-09-25 20:36:24 -0400

    popup added

---
+++ src/components/Modal/index.js
@@ -0,0 +1,33 @@
+import './index.scss';
+import BitcoinPayment from '../BitcoinPayment';
+
+function Modal({product, setShowModal}) {
+  return (
+  <div className="Modal">
+    <div className="modal_inner">
+        <div className="close_wrapper">
+          <span
+            className="close_icon"
+            onClick={() => { setShowModal(false) }}>x</span>
+        </div>
+        <div className="content_wrapper">
+          <h2 className="artist_block">
+            <strong>{product.artist}</strong>
+          </h2>
+
+          <img src={product.image} alt={product.artist} className="event_image"/>
+
+          <p className="date_time_block">
+            <strong>{product.date} - {product.time}</strong>
+          </p>
+
+          <p className="venue_block">{product.venue} - {product.location}</p>
+
+          <BitcoinPayment />
+        </div>
+    </div>
+  </div>
+  );
+}
+
+export default Modal;
--- src/components/ProductList/index.js
+++ src/components/ProductList/index.js
@@ -1,12 +1,15 @@
 import './index.scss';
 import Product from '../Product';
 
-function ProductList({ products }) {
+function ProductList({ products, displayProduct }) {
   return (
   <div className="ProductList">
     {
       products.map(function(product, index) {
-        return <Product product={product} key={index} />;
+        return <Product
+          product={product}
+          key={index}
+          displayProduct={displayProduct} />;
       })
     }
   </div>
